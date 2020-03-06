a1d6f7a3c7dddfaf8e97017a6bbaa6729776bd7e
Barton <huiwenbo666@163.com>
2020-03-06 18:22:59 +0800

    Add 4 tabbar & login page

--- miniprogram/pages/login/login.js
+++ miniprogram/pages/login/login.js
@@ -2,163 +2,65 @@
 Page({
 
   data: {
-
     phone: '',
-
     password: '',
-
     success: false,
-
     text: ''
-
- 
-
   },
-
- 
-
   // 获取输入账号 
-
   phoneInput: function (e) {
-
     this.setData({
-
       phone: e.detail.value
-
     })
-
   },
-
- 
-
   // 获取输入密码 
 
   passwordInput: function (e) {
-
     this.setData({
-
       password: e.detail.value
 
     })
 
   },
 
- 
-
   // 登录 
-
   login: function () {
-
     var that = this;   
-
- 
-
-    var warn = null; //warn为当手机号为空或格式不正确时提示用户的文字，默认为空
-
+    var warn = "哼，你是二逼"; 
     if (that.data.phone.length == 0) {
-
       wx.showToast({
-
         title: '用户名不能为空',
-
         icon: 'loading',
-
         duration: 1000
-
       })
 
     } else if (that.data.password.length == 0) {
-
       wx.showToast({
-
         title: '密码不能为空',
-
         icon: 'loading',
-
         duration: 1000
-
       })
 
     }else {
-
-      
-
-      wx.request({
-
-        url: 'http://192.168.41.40:8002/login',
-
-        method: "POST",
-
-        data: {
-
-          cardNo: that.data.phone,
-
-          password: that.data.password
-
-        },
-
-        header: {
-
-          'content-type': 'application/x-www-form-urlencoded'
-
-        },
-
-        success: function (res) {
-
-          if (res.data.state == 1) {  //判断是否能正常登录
-
-            warn = "卡号密码不匹配";
-
-            wx.showModal({
-
-              title: '提示',
-
-              content: warn
-
-            })
-
-            return;
-
-          } else {
-
-            that.setData({
-
-              success: true,
-
-              text: res.data.url
-
-            })
-
-          }
-
-        }
-
- 
-
-      })
-
- 
-
- 
-
- 
-
+      if(that.data.phone == "ethan" && that.data.password == "ethan"){
+        console.log("ssss")
+        wx.switchTab({
+          url: '/pages/picture/picture',
+        })
+      }else{
+        wx.showModal({
+          title: '提示',
+          content: warn
+        })
+      }
     }
-
   },
-
-  // 注册 
-
-  register: function () {
-
-    wx.navigateTo({
-
-      url: '/pages/register/register',
-
+  // 清空
+  cleanName: function () {
+    this.setData({
+      phone : "",
+      passWord : ""
     })
-
   }
-
- 
 
 })
---
+++ miniprogram/pages/picture/picture.js
@@ -0,0 +1,87 @@
+// pages/picture/picture.js
+Page({
+
+  /**
+   * Page initial data
+   */
+  data: {
+    pictureArray : [
+      {
+        text:'好认真的样子啊',
+        src:'../../images/picture/mm1.jpg',
+        date:'2020/03/06'
+      },
+      {
+        text:'看我的小乌龟',
+        src:'../../images/picture/mm2.jpg',
+        date:'2020/03/06'
+      },
+      {
+        text:'吐舌头呢',
+        src:'../../images/picture/mm3.jpg',
+        date:'2020/03/06'
+      },
+      {
+        text:'吐舌头呢',
+        src:'../../images/picture/mm3.jpg',
+        date:'2020/03/06'
+      }
+    ]
+  },
+
+  /**
+   * Lifecycle function--Called when page load
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page is initially rendered
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page show
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page hide
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page unload
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * Page event handler function--Called when user drop down
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * Called when page reach bottom
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * Called when user click on the top right corner to share
+   */
+  onShareAppMessage: function () {
+
+  }
+})
